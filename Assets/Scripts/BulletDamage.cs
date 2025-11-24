f4a3b95f0b83842b1e15b69813cb58fb9bfd1eca
Ickle1790 <Donnelly-F16@ulster.ac.uk>
2025-11-24 22:42:25 +0000

    2D, can now kill enemies
    24/11/25, 10:42pm

---
+++ Assets/Scripts/BulletDamage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    public int bulletDamage;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<EnemyHealth>().damageEnemy(bulletDamage);
+            Destroy(gameObject);
+        }
+    }
+}
