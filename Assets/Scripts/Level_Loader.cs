646009d08a2b2fa2d2d4e39f66b9da55fea2935d
Ickle1790 <Donnelly-F16@ulster.ac.uk>
2025-11-25 00:19:36 +0000

    2D, added scene movement
    25/11/25, 12:19am

---
+++ Assets/Scripts/Level_Loader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Loader : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "LevelExit")
+        {
+           // SceneManager.LoadScene(1)
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
