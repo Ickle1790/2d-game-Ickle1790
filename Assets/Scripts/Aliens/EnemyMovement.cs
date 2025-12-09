78a5a8e7bf5c92a5fa5902795aac3d085e6aa2f7
Finn Donnelly <Donnelly-F16@ulster.ac.uk>
2025-12-09 12:58:33 +0000

    failed at enemy movement

--- Assets/Scripts/Aliens/EnemyMovement.cs
+++ Assets/Scripts/Aliens/EnemyMovement.cs
@@ -1,16 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private int startDirection = 1;
+    private int currentDirection;
+    private float halfWidth;
+    private Vector2 movement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        halfWidth = spriteRenderer.bounds.extents.x;
+        currentDirection = startDirection;
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        movement.x = speed * currentDirection;
+        movement.y = rigidBody.linearVelocity.y;
+        rigidBody.linearVelocity = movement;
+        SetDirection();
+    }
+
+    private void SetDirection()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.right, halfWidth + 0.1f, LayerMask.GetMask("Ground")) && rigidBody.linearVelocityX > 0);
+        {
+            // Draw a ray starting at the centre of our enemy and point it to the right
+            // Check to see if the raycast is intersecting with a wall
+            // Check to make sure the enemy is walking right
+            // If this doesn't happen, the enemy will be constantly moving back and forth
+            currentDirection *= -1;
+        }
+        Debug.DrawRay(transform.position, Vector2.right * (halfWidth + 0.1f), Color.red);
     }
 }
